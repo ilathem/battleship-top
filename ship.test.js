af8e43d5c0ae4703113861bae224ad10039b01ff
ilathem <isaiahlathem@gmail.com>
2024-06-24 21:30:04 -0500

    write test for ship hit function

--- ship.test.js
+++ ship.test.js
@@ -4,8 +4,19 @@ test('ship creation', () => {
   const testShip = {
     length: 2,
     hitCount: 0,
-    sunk: false
-  }
+    sunk: false,
+  };
   const ship = new Ship(2);
   expect(testShip).toEqual(ship);
-})
+});
+
+test('hit function', () => {
+  const testShip = {
+    length: 2,
+    hitCount: 1,
+    sunk: false,
+  };
+  const ship = new Ship(2);
+  ship.hit();
+  expect(testShip).toEqual(ship);
+});
