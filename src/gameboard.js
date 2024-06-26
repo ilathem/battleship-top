96d0ac2193b8ca1e562483227f1734fa5664e053
ilathem <isaiahlathem@gmail.com>
2024-06-26 03:41:28 -0500

    implement placeship

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,6 +1,32 @@
+const Ship = require('./ship');
+
 class Gameboard {
   constructor() {
     this.board = Array.from(Array(10), () => new Array(10));
+  }
+
+  placeShip(startX, startY, endX, endY) {
+    if (startX !== endX && startY !== endY)
+      throw new Error('No diagonal ships');
+    let orientation = undefined;
+    if (startY === endY) orientation = 'horizontal';
+    if (startX === endX) orientation = 'vertical';
+    let length = undefined;
+    if (orientation === 'horizontal') {
+      length = Math.abs(startY - endY);
+    } else if (orientation === 'vertical') {
+      length = Math.abs(startX - endX);
+    }
+    let constantCoord = orientation === 'horizontal' ? startY : startX;
+    if (orientation === 'horizontal') {
+      for (let i = startX; i <= endX; i++) {
+        this.board[i][constantCoord] = 'S';
+      }
+    } else if (orientation === 'vertical') {
+      for (let i = startY; i <= endY; i++) {
+        this.board[constantCoord][i] = 'S';
+      }
+    }
   }
 }
 
