18283ec949e185504688cbb320c1fe3217396d4d
ilathem <isaiahlathem@gmail.com>
2024-06-26 03:55:15 -0500

    add test for reverse coordinates

--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -17,6 +17,18 @@ test('Ship placement happy case', () => {
   testboard[0][2] = 'S';
   testboard[0][3] = 'S';
   board.placeShip(0, 0, 0, 3);
+  expect(board.board).toEqual(testboard);
+})
+
+test('Reverse coordinates work with ship placements', () => {
+  let testboard = Array.from(Array(10), () => new Array(10));
+  const board = new Gameboard();
+  testboard[0][0] = 'S';
+  testboard[0][1] = 'S';
+  testboard[0][2] = 'S';
+  testboard[0][3] = 'S';
+  board.placeShip(0, 3, 0, 0);
+  expect(board.board).toEqual(testboard);
 })
 
 test('Diagonal coordinates throws', () => {
