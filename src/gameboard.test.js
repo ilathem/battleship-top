cfecc8b81339dd49cdbd0fca05e85a5881510af4
ilathem <isaiahlathem@gmail.com>
2024-06-26 03:24:07 -0500

    write test for ship placement

--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -8,3 +8,14 @@ test('Gameboard creation', () => {
   const board = new Gameboard();
   expect(testGameboard).toEqual(board);
 })
+
+test('Ship placement', () => {
+  let testboard = Array.from(Array(10), () => new Array(10));
+  const board = new Gameboard();
+  testboard[0][0] = 'S';
+  testboard[0][1] = 'S';
+  testboard[0][2] = 'S';
+  testboard[0][3] = 'S';
+  board.placeShip(0, 0, 0, 3);
+  expect(board.board).toEqual(testboard);
+})
