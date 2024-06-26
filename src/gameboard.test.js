d8cdb91f5ab11ac70e965edc05709167b934e0aa
ilathem <isaiahlathem@gmail.com>
2024-06-26 03:44:17 -0500

    write test for no diagonal ships

--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -9,7 +9,7 @@ test('Gameboard creation', () => {
   expect(testGameboard).toEqual(board);
 })
 
-test('Ship placement', () => {
+test('Ship placement happy case', () => {
   let testboard = Array.from(Array(10), () => new Array(10));
   const board = new Gameboard();
   testboard[0][0] = 'S';
@@ -17,5 +17,8 @@ test('Ship placement', () => {
   testboard[0][2] = 'S';
   testboard[0][3] = 'S';
   board.placeShip(0, 0, 0, 3);
-  expect(board.board).toEqual(testboard);
+})
+
+test('Diagonal coordinates throws', () => {
+  expect(() => new Gameboard(0, 0, 1, 3)).toThrow('No diagonal ships');
 })
