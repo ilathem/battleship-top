5eaa49f1b158e153aa770476dcf08ffebfe23ecd
ilathem <isaiahlathem@gmail.com>
2024-11-04 08:23:09 -0600

    show where ships will be placed

--- src/driver.js
+++ src/driver.js
@@ -1,26 +1,25 @@
-const Player = require('./player');
+const Player = require("./player");
 
 function populateWithPredeterminedCoordinates(player) {
   const shipTemplate = [
-    [0, 0, 0, 4, 'carrier'],
-    [1, 9, 4, 9, 'battleship'],
-    [1, 0, 1, 2, 'destroyer'],
-    [6, 5, 8, 5, 'submarine'],
-    [9, 0, 9, 1, 'patrol boat'],
-  ]
+    [0, 0, 0, 4, "carrier"],
+    [1, 9, 4, 9, "battleship"],
+    [1, 0, 1, 2, "destroyer"],
+    [6, 5, 8, 5, "submarine"],
+    [9, 0, 9, 1, "patrol boat"],
+  ];
   for (const ship in shipTemplate) {
-    player.board.placeShip(...shipTemplate[ship])
+    player.board.placeShip(...shipTemplate[ship]);
   }
 }
 
 function createTemplateGame() {
-  const player1 = new Player('real');
-  const player2 = new Player('real');
+  const player1 = new Player("real");
+  const player2 = new Player("real");
   // populateWithPredeterminedCoordinates(player1);
   // populateWithPredeterminedCoordinates(player2);
-  return { player1, player2 }
+  return { player1, player2 };
 }
-
 
 function renderGameBoard(container, player, triggerNextTurn, msg) {
   // console.log(player.board);
@@ -31,13 +30,13 @@ function renderGameBoard(container, player, triggerNextTurn, msg) {
     htmlTable[i] = new Array(player.board.board[i].length);
     for (let j = 0; j < player.board.board[i].length; j++) {
       const tableData = document.createElement("td");
-      let displayText = player.board.board[i][j] || '-';
+      let displayText = player.board.board[i][j] || "-";
       // if (['X', 'O'].includes(player.board.board[i][j])) displayText = player.board.board[i][j];
       // else displayText = '-'
       tableData.innerText = displayText;
       htmlTable[i][j] = tableData;
-      tableData.addEventListener('click', () => {
-        if (['X', 'O'].includes(player.board.board[i][j])) {
+      tableData.addEventListener("click", () => {
+        if (["X", "O"].includes(player.board.board[i][j])) {
           // console.log('this has already been attacked');
           return;
         }
@@ -47,8 +46,8 @@ function renderGameBoard(container, player, triggerNextTurn, msg) {
         // console.log(result);
         tableData.innerText = player.board.board[i][j];
         msg.innerText = result;
-        setTimeout(triggerNextTurn, 100)
-      })
+        setTimeout(triggerNextTurn, 100);
+      });
       row.appendChild(tableData);
     }
     table.appendChild(row);
@@ -57,86 +56,91 @@ function renderGameBoard(container, player, triggerNextTurn, msg) {
   return htmlTable;
 }
 
+const ships = [
+  {
+    name: "Aircraft Carrier",
+    length: 5,
+  },
+  {
+    name: "Battleship",
+    length: 4,
+  },
+  {
+    name: "Destroyer",
+    length: 4,
+  },
+  {
+    name: "Submarine",
+    length: 3,
+  },
+  {
+    name: "Patrol Boat",
+    length: 2,
+  },
+];
+
 function renderShips(player, activeShip, htmlTable) {
-  let direction = 'horizontal';
-  const div = document.createElement('div');
-  div.classList.add('renderShipsDiv');
-  const p = document.createElement('p');
-  p.innerText = 'Select which ship you would like to place';
-  p.classList.add('selectionText');
-  const shipsDiv = document.createElement('div');
-  shipsDiv.classList.add('shipsToChooseDiv');
-  const input = document.createElement('input');
-  input.classList.add('shipLocationInput');
-  input.type = 'number';
-  input.placeholder = 'Tap or type starting square';
+  let direction = "horizontal";
+  const div = document.createElement("div");
+  div.classList.add("renderShipsDiv");
+  const p = document.createElement("p");
+  p.innerText = "Select which ship you would like to place";
+  p.classList.add("selectionText");
+  const shipsDiv = document.createElement("div");
+  shipsDiv.classList.add("shipsToChooseDiv");
+  const input = document.createElement("input");
+  input.classList.add("shipLocationInput");
+  input.type = "number";
+  input.placeholder = "Tap or type starting square";
   input.disabled = true;
-  const inputDiv = document.createElement('div');
-  inputDiv.classList.add('inputDiv');
-  const rotateBtn = document.createElement('button');
-  rotateBtn.innerText = 'Rotate ship';
-  rotateBtn.classList.add('rotateBtn');
-  rotateBtn.addEventListener('click', () => {
-    if (direction === 'horizontal') direction = 'vertical';
-    else direction = 'horiztonal';
-  })
-  const ships = [
-    {
-      name: 'Aircraft Carrier',
-      length: 5,
-    },
-    {
-      name: 'Battleship',
-      length: 4,
-    },
-    {
-      name: 'Destroyer',
-      length: 4,
-    },
-    {
-      name: 'Submarine',
-      length: 3,
-    },
-    {
-      name: 'Patrol Boat',
-      length: 2,
-    }
-  ];
+  const inputDiv = document.createElement("div");
+  inputDiv.classList.add("inputDiv");
+  const rotateBtn = document.createElement("button");
+  rotateBtn.innerText = "Rotate ship";
+  rotateBtn.classList.add("rotateBtn");
+  rotateBtn.addEventListener("click", () => {
+    if (direction === "horizontal") direction = "vertical";
+    else direction = "horiztonal";
+  });
   const shipBtnDivs = [];
   for (let i = 0; i < 5; i++) {
-    const shipBtn = document.createElement('div');
+    const shipBtn = document.createElement("div");
     shipBtnDivs.push(shipBtn);
     shipBtn.innerText = ships[i].name;
-    shipBtn.classList.add('shipBtn');
+    shipBtn.classList.add("shipBtn");
     shipsDiv.appendChild(shipBtn);
-    shipBtn.addEventListener('click', () => {
-      activeShip = ships[i].name;
+    shipBtn.addEventListener("click", () => {
+      console.log(`setting active ship to ${ships[i].name}`);
+      activeShip.innerText = ships[i].name;
       input.disabled = false;
-      input.style.border = '2px solid #00B4EA';
-      shipBtnDivs.forEach(btn => btn.style.border = '2px solid white')
-      shipBtn.style.border = '2px solid #00B4EA';
-      rotateBtn.style.border = '2px solid #00B4EA';
-    })
+      input.style.border = "2px solid #00B4EA";
+      shipBtnDivs.forEach((btn) => (btn.style.border = "2px solid white"));
+      shipBtn.style.border = "2px solid #00B4EA";
+      rotateBtn.style.border = "2px solid #00B4EA";
+    });
   }
-  input.addEventListener('keyup', e => {
-    if (e.target.value.toString().length < 2 ||
-        e.target.value.toString().length > 2 ||
-        e.target.value < 0 ||
-        e.target.value > 99 ||
-        e.target.value.toString().includes('-') ||
-        e.target.value.toString().includes('.')) return;
-    console.log(e.target.value)
+  input.addEventListener("keyup", (e) => {
+    if (
+      e.target.value.toString().length < 2 ||
+      e.target.value.toString().length > 2 ||
+      e.target.value < 0 ||
+      e.target.value > 99 ||
+      e.target.value.toString().includes("-") ||
+      e.target.value.toString().includes(".")
+    )
+      return;
+    console.log(e.target.value);
     console.log(activeShip);
     const row = Number(e.target.value.toString()[0]);
     const column = Number(e.target.value.toString()[1]);
-    const shipData = ships.find(ship => ship.name === activeShip);
-    console.table(row, column, shipData)
-    if (direction === 'horizontal') {
+    const shipData = ships.find((ship) => ship.name === activeShip);
+    console.table(row, column, shipData);
+    if (direction === "horizontal") {
       for (let i = 0; i < shipData.length; i++) {
-        htmlTable[row][column + i].innerText = '0'; 
+        htmlTable[row][column + i].innerText = "0";
       }
     }
-  })
+  });
   div.appendChild(p);
   div.appendChild(shipsDiv);
   inputDiv.appendChild(input);
@@ -147,7 +151,9 @@ function renderShips(player, activeShip, htmlTable) {
 
 function renderPregameBoard(container, player) {
   const htmlTable = [];
-  let activeShip = null;
+  let activeShip = document.createElement("p");
+  activeShip.style.display = "none";
+  container.appendChild(activeShip);
   const table = document.createElement("table");
   for (let i = 0; i < player.board.board.length; i++) {
     const row = document.createElement("tr");
@@ -157,9 +163,35 @@ function renderPregameBoard(container, player) {
       let displayText = player.board.board[i][j] || `${i}${j}`;
       tableData.innerText = displayText;
       htmlTable[i][j] = tableData;
-      tableData.addEventListener('mouseup', () => {
-        console.log(`selected coordinate (${i}, ${j})`)
-      })
+      tableData.addEventListener("mouseover", () => {
+        const length =
+          ships.find((ship) => ship.name === activeShip.innerText).length || 0;
+        console.log(`mousing over for ${activeShip.innerText}`);
+        if (j + length > 10) {
+          htmlTable[i][j].classList.add("invalid");
+          return;
+        }
+        htmlTable[i][j].classList.add("valid");
+        for (let k = j; k < j + length; k++) {
+          htmlTable[i][k].classList.add("shipPlacementMouseOver");
+        }
+      });
+      tableData.addEventListener("mouseleave", () => {
+        const length =
+          ships.find((ship) => ship.name === activeShip.innerText).length || 0;
+        console.log(`mousing leave for ${activeShip.innerText}`);
+        htmlTable[i][j].classList.remove("invalid");
+        htmlTable[i][j].classList.remove("valid");
+        if (j + length > 10) {
+          return;
+        }
+        for (let k = j; k < j + length; k++) {
+          htmlTable[i][k].classList.remove("shipPlacementMouseOver");
+        }
+      });
+      tableData.addEventListener("mouseup", () => {
+        console.log(`selected coordinate (${i}, ${j})`);
+      });
       row.appendChild(tableData);
     }
     table.appendChild(row);
@@ -169,4 +201,4 @@ function renderPregameBoard(container, player) {
   return htmlTable;
 }
 
-module.exports = { createTemplateGame, renderGameBoard, renderPregameBoard } 
+module.exports = { createTemplateGame, renderGameBoard, renderPregameBoard };
